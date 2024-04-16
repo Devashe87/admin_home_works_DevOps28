3eb83c5f7bbcad74fe6e5982c2996e0e64e77978
Devashe87 <devashe87@MacBook-Pro-John.local>
2024-04-16 19:15:50 +0300

    Home work is done

---
+++ 09-ci-03-cicd/Src/main.tf
@@ -0,0 +1,74 @@
+locals {
+  ssh-keys = file("~/.ssh/centos7.pub")
+}
+
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+
+data "yandex_compute_image" "centos7" {
+  family = var.vm_centos7
+}
+
+resource "yandex_compute_instance" "sonar-01" {
+  name        = "sonar-01"
+  platform_id = var.vm_platform_id
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 20
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.centos7.image_id
+    }
+  }
+  
+  metadata = {
+  ssh-keys = "centos:${local.ssh-keys}"
+  serial-port-enable = "1"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+}
+
+resource "yandex_compute_instance" "nexus-01" {
+  name        = "nexus-01"
+  platform_id = var.vm_platform_id
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 20
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.centos7.image_id
+    }
+  }
+  
+  metadata = {
+  ssh-keys = "centos:${local.ssh-keys}"
+  serial-port-enable = "1"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+}
---
+++ 09-ci-03-cicd/Src/vms_platform.tf
@@ -0,0 +1,11 @@
+variable "vm_centos7" {
+  type = string
+  default = "centos-7"
+  description = "Os version"
+}
+
+variable "vm_platform_id" {
+  type = string
+  default = "standard-v3"
+  description = "platform ID"
+}
